32658f8334af9307eab9a17f8713200d1ceb35a4
Jun220 <152267292+jun220@users.noreply.github.com>
2024-02-25 06:39:24 +0900

    Title: 지그재그로 숫자 채우기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240225/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int num=0;
+    vector<vector<int>>v(n, vector<int>(n));
+
+    for(int j=0; j<m;j++){
+        if(j%2 == 0){
+            for(int i=0;i<n;i++){
+                v[j][i] = num++;
+            }
+        }
+        else{
+            for(int i=n-1;i>=0;i--){
+                v[j][i] = num++;
+            }
+        }
+    }
+
+    for(int j=0;j<n;j++){
+        for(int i=0;i<m;i++){
+            cout << v[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+
+    return 0;
+}
