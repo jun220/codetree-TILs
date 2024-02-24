d18cde55b78d986806b6d7128b3b43e1e8f866f1
Jun220 <152267292+jun220@users.noreply.github.com>
2024-02-25 03:46:52 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v1(n, vector<int>(m, 0));
+    vector<vector<int>> v2(n, vector<int>(m, 0));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> v1[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> v2[i][j];
+        }
+    }
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(v1[i][j] == v2[i][j]) cout << 0;
+            else cout << 1;
+
+            cout << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
