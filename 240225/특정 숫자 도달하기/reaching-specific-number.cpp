837122bb34fd1c8305b0089bb2ac0f726e9e961e
Jun220 <152267292+jun220@users.noreply.github.com>
2024-02-25 02:53:35 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int temp;
+    vector<int> v;
+    int sum{0}; 
+    float average;
+    for(int i=0;i<10;i++){
+        cin >> temp;
+        if(temp >= 250) {
+            break;
+        }
+        v.push_back(temp);
+    }
+
+    for(int i=0;i<v.size();i++){
+        sum += v[i];
+    }
+    average = static_cast<float>(sum) / v.size();
+    cout << sum << ' ' << average;
+    return 0;
+}
