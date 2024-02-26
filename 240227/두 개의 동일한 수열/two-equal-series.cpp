d32389cedc528e1f2932ff667f03cd7936850935
Jun220 <152267292+jun220@users.noreply.github.com>
2024-02-27 02:07:09 +0900

    Title: 두 개의 동일한 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool check(vector<int> v1, vector<int> v2){
+    for(int i=0;i<v1.size(); i++){
+        if(v1[i] != v2[i]){
+            cout << i << v1[i] << v2[i];
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v1(n, 0);
+    vector<int> v2(n, 0);
+    for(int i=0;i<n;i++){
+        cin >> v1[i];
+    }
+    for(int i=0;i<n;i++){
+        cin >> v2[i];
+    }
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    
+
+    if(check(v1, v2)) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
