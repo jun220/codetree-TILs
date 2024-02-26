ac486fa78dcaaf9212200b5d76b5087e118630ab
Jun220 <152267292+jun220@users.noreply.github.com>
2024-02-27 01:56:11 +0900

    Title: Top K 숫자 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, k;
+    cin >> N >> k;
+    vector<int> v(N, 0);
+    
+    for(int i=0; i<N; i++){
+        cin >> v[i];
+    }
+
+    cout << v[k-1];
+    
+    return 0;
+}
