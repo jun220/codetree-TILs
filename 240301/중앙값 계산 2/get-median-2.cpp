bbdbea11decf08d523254ecc3b26e9c7e740cc82
Jun220 <152267292+jun220@users.noreply.github.com>
2024-03-01 05:28:48 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// void print(vector<int> &v, int n){
+//     sort(v.begin(), v.end());
+//     for(int i=0;i<n;i++){
+//         cout << v[i] << ' ';
+//     }
+//     cout << '\n';
+// }
+
+int mid(vector<int> v, int num){
+    sort(v.begin(), v.end());
+    //print(v, num);
+    //cout << "index : " << num/2 << '\n';
+    return v[num/2];
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n, 101);
+
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+        //print(v, i);
+
+        if(i % 2 == 0){
+            cout << mid(v, i+1) << ' ';
+        }
+    }
+
+    return 0;
+}
