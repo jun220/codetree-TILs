608efa49b01fabba1aff833737e361c59eb3c009
Jun220 <152267292+jun220@users.noreply.github.com>
2024-03-01 05:32:04 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240301/중앙값 계산 2/get-median-2.cpp
+++ 240301/중앙값 계산 2/get-median-2.cpp
@@ -1,36 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
-
-// void print(vector<int> &v, int n){
-//     sort(v.begin(), v.end());
-//     for(int i=0;i<n;i++){
-//         cout << v[i] << ' ';
-//     }
-//     cout << '\n';
-// }
-
-int mid(vector<int> v, int num){
-    sort(v.begin(), v.end());
-    //print(v, num);
-    //cout << "index : " << num/2 << '\n';
-    return v[num/2];
-}
-
-
 
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n, 200);
 
-    for(int i=0;i<n;i++){
-        cin >> v[i];
-        //print(v, i);
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    vector<int> numbers(n);
 
-        if(i % 2 == 0){
-            cout << mid(v, i+1) << ' ';
+    for(int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+
+        if(i % 2 == 0) {
+            max_heap.push(numbers[i]);
+            if(min_heap.empty() || max_heap.top() > min_heap.top()) {
+                min_heap.push(max_heap.top());
+                max_heap.pop();
+            }
+            cout << min_heap.top() << " ";
         }
     }
 
