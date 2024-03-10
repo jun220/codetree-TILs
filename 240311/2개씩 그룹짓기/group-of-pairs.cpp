4ee9ced99c8c68e8912ff9711a4917af27cd916c
Jun220 <152267292+jun220@users.noreply.github.com>
2024-03-11 01:53:49 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240311/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int N;
+    cin >> N;
+    vector<int> v(2 * N);
+    vector<int> sum(N);
+    for (int i = 0; i < 2 * N; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i <= N - 1; i++) {
+        sum[i] = v[i] + v[2 * N - 1 - i];
+    }
+    
+    cout << *max_element(sum.begin(), sum.end());
+    
+    return 0;
+}
