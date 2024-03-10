266f3eef2120387bf70eb91ca7876c75538f32bd
Jun220 <152267292+jun220@users.noreply.github.com>
2024-03-11 02:14:40 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 240311/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 bool isSameWord(string s1, string s2){
+    if(s1.size() != s2.size()) return false;
     for(int i=0;i<s1.size();i++){
         if(s1[i] != s2[i]) return false;
     }
