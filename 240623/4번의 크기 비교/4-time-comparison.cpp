11f75968ba15421062f92616afd1b9316175df92
Jun220 <152267292+jun220@users.noreply.github.com>
2024-06-23 00:25:03 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240623/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a;
+    cin >> b >> c >> d >> e;
+    if(a>b)cout << 1;
+    else cout << 0;
+    cout <<'\n';
+    if(a>c)cout << 1;
+    else cout << 0;
+    cout <<'\n';
+    if(a>d)cout << 1;
+    else cout << 0;
+    cout <<'\n';
+    if(a>e)cout << 1;
+    else cout << 0;
+    cout <<'\n';
+    return 0;
+}
